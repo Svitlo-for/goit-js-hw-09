98adae25ccd02fae473e7fc3bf8b2629294693ac
Svitlo-for <serg_machigin@ukr.net>
2023-08-15 23:07:34 +0300

    add-task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
