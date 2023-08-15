fb67c332282e2836a02be0f3b88f83ef13995aae
Svitlo-for <127992752+Svitlo-for@users.noreply.github.com>
2023-08-15 20:07:57 +0000

    Deploying to gh-pages from @ Svitlo-for/goit-js-hw-09@98adae25ccd02fae473e7fc3bf8b2629294693ac 🚀

---
+++ 01-color-switcher.27149974.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(()=>{e.disabled=!0,o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),a.addEventListener("click",(()=>{e.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.27149974.js.map
