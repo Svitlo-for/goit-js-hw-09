01bf505ce4c79d795cf687c8e4cecdda1a9bcf22
Svitlo-for <127992752+Svitlo-for@users.noreply.github.com>
2023-08-20 22:02:39 +0300

    correct-task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,11 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const inputPicker = document.getElementById("datetime-picker");
 const startBtn = document.querySelector("input[data-start]");
+
+const timerDate = document.querySelector("span[data-days]");
 
 const options = {
     enableTime: true,
@@ -10,7 +13,13 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+      console.dir(selectedDates[0]);
+      const currentTime = Date.now();
+      if (selectedDates[0].getTime < currentTime) {
+        Notiflix.Report.warning('Please choose a date in the future');
+      } else {
+        
+      }
     },
   };
 
